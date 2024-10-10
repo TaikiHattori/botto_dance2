7acb286f6f7fb6c4b0a72c06012ca8f5ba883a2c
TaikiHattori <tvajarvis@outlook.jp>
2024-10-10 23:32:03 +0900

    S3アップロードDB連携＆一覧＆削除追加

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UploadController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +18,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// 🔽 追加
+    Route::resource('uploads', UploadController::class);
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::post('/upload', [UploadController::class, 'store'])->name('upload');
+Route::get('/uploads', [UploadController::class, 'index'])->name('uploads.index');
+
+Route::get('/extractions/create/{upload_id}', [ExtractionController::class, 'create'])->name('extractions.create');
