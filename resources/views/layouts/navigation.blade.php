037fe2445baa8c537af1494f734b49a3d7b1953a
TaikiHattori <tvajarvis@outlook.jp>
2024-11-12 00:00:49 +0900

    抽出再生

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('playlists.create')" :active="request()->routeIs('playlists.create')">
                         {{ __('botto') }}
                     </x-nav-link>
                     <x-nav-link :href="route('uploads.create')" :active="request()->routeIs('uploads.create')">
@@ -76,7 +76,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('playlists.create')" :active="request()->routeIs('playlists.create')">
                 {{ __('botto') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('uploads.create')" :active="request()->routeIs('uploads.create')">
