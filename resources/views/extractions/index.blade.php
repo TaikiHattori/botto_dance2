1d88300e83d802e542fb3ef8121175bcd7ae72b6
TaikiHattori <tvajarvis@outlook.jp>
2024-12-24 23:34:24 +0900

    抽出一覧画面更新

--- resources/views/extractions/index.blade.php
+++ resources/views/extractions/index.blade.php
@@ -1,6 +1,7 @@
 <div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
 </div>
+
 
 <x-app-layout>
   <x-slot name="header">
@@ -9,7 +10,53 @@
     </h2> -->
   </x-slot>
 
-  <style>
+<style>
+    html, body {
+      color: #ffffff; /* テキスト色を白に */
+      margin: 0;
+      padding: 0;
+      height: 100%;
+    }
+
+    .text-gray-800, .text-gray-900, .dark\:text-gray-100 {
+      color: #ffffff !important; /* テキスト色を白に */
+    }
+
+    .bg-blue-500 {
+      background-color: #333333 !important; /* ボタンの背景色を#333333に設定 */
+    }
+
+    .hover\:bg-blue-700:hover {
+      background-color: #444444 !important; /* ボタンのホバー時の背景色を#444444に設定 */
+    }
+    .container {
+      min-height: 100vh; /* コンテナの高さを画面全体に */
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .min-h-screen {
+      min-height: 100vh; /* 画面全体の高さを確保 */
+    }
+
+    .bg-gray-100 {
+      background-color: #1f1f1f !important; /* 背景色を#1f1f1fに設定 */
+      border: 1px solid #ffffff !important; /* 枠線の色を白に設定 */
+    }
+
+    .dark\:bg-gray-900 {
+      background-color: #1f1f1f !important; /* ダークモードの背景色を#1f1f1fに設定 */
+    }
+
+    .text-blue-500 {
+      color: #1e90ff !important; /* リンクのテキスト色を青に設定 */
+    }
+
+    .hover\:text-blue-700:hover {
+      color: #1c86ee !important; /* リンクのホバー時のテキスト色を濃い青に設定 */
+    }
   </style>
 
   {{-- <div class="py-12">
@@ -36,7 +83,7 @@
   <div class="py-12 px-4">
     @foreach ($extractions as $extraction)
     <div class="flex max-w-md mx-auto overflow-hidden rounded-lg shadow-lg mb-4" style="box-shadow: 0px 0px 30px 10px rgb(255 255 255 / 80%);">
-        <div class="w-1/3  bg-no-repeat bg-contain bg-center" style="background-image: url('{{ asset('images/tsuki2.png') }}')"></div>
+        <div class="w-1/3  bg-no-repeat bg-contain bg-center" style="background-image: url('{{ asset('storage/images/tsuki2.png') }}')"></div>
     
         <div class="w-2/3 p-4 md:p-4">
             <p class="text-xm font-bold text-white">{{ $extraction->upload->title }}</p>
@@ -45,8 +92,8 @@
 
             <div class="flex justify-between mt-3 item-center">
                 <a href="{{ route('extractions.show', $extraction) }}" class="text-sm hover:text-gray-200">詳細を見る</a>
-            
-            </div>
+
+              </div>
         </div>
     </div>
     @endforeach
