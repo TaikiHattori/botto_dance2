9d2c666b97ca0dd7cff01f91bf48d308b67a10d2
TaikiHattori <tvajarvis@outlook.jp>
2025-01-23 13:40:34 +0900

    何分何秒化

--- resources/views/extractions/index.blade.php
+++ resources/views/extractions/index.blade.php
@@ -59,27 +59,6 @@
     }
   </style>
 
-  {{-- <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900 dark:text-gray-100">
-          @foreach ($extractions as $extraction)
-          <div class="mb-4 p-4 rounded-lg">
-            <p class="text-gray-800 dark:text-gray-300">タイトル: {{ $extraction->upload->title }}</p>
-            <p class="text-gray-600 dark:text-gray-400 text-sm">抽出開始: {{ $extraction->start }}</p>
-            <p class="text-gray-600 dark:text-gray-400 text-sm">抽出終了: {{ $extraction->end }}</p>
-            <a href="{{ route('extractions.show', $extraction) }}" class="text-blue-500 hover:text-blue-700">詳細を見る</a>
-
-            <a href="{{ route('memos.create', $extraction) }}" class="ml-4 text-blue-500 hover:text-blue-700">メモ作成</a>
-
-
-          </div>
-          @endforeach
-        </div>
-      </div>
-    </div>
-  </div> --}}
-
   <div class="py-12 px-4">
     @foreach ($extractions as $extraction)
     <div class="flex max-w-md mx-auto overflow-hidden rounded-lg shadow-lg mb-4" style="box-shadow: 0px 0px 30px 10px rgb(255 255 255 / 80%);">
@@ -87,8 +66,8 @@
     
         <div class="w-2/3 p-4 md:p-4">
             <p class="text-xm font-bold text-white">{{ $extraction->upload->title }}</p>
-            <p class="text-white text-sm">開始: {{ $extraction->start }}</p>
-            <p class="text-white text-sm">終了: {{ $extraction->end }}</p>
+            <p class="text-white text-sm">開始: {{ substr($extraction->start, 3) }}</p>
+            <p class="text-white text-sm">終了: {{ substr($extraction->end, 3) }}</p>
 
             <div class="flex justify-between mt-3 item-center">
                 <a href="{{ route('extractions.show', $extraction) }}" class="text-sm hover:text-gray-200">詳細を見る</a>
