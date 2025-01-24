12a2b5bb815889c43bf4509ce3bd79c9a4057858
TaikiHattori <tvajarvis@outlook.jp>
2025-01-24 12:03:09 +0900

    プレイリスト編集（何分何秒）

--- resources/views/extractions/edit.blade.php
+++ resources/views/extractions/edit.blade.php
@@ -13,15 +13,24 @@
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100">
           <a href="{{ route('extractions.show', $extraction) }}" class="text-blue-500 hover:text-blue-700 mr-2">詳細に戻る</a>
+          
           <form method="POST" action="{{ route('extractions.update', $extraction) }}">
             @csrf
             @method('PUT')
             
             <div class="mb-4 text-black">
-            <p>開始（秒）</p>
-            <input type="number" name="start" id="start" value="{{ $extraction->start }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-            <p>終了（秒）</p>
-            <input type="number" name="end" id="end" value="{{ $extraction->end }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              <p>開始</p>
+              <div class="flex">
+              <input type="number" name="start_minu" id="start_minu" value="{{ $extraction->start }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="分">
+              <input type="number" name="start_sec" id="start_sec" value="{{ $extraction->start }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="秒">
+              </div>
+            
+              <p>終了</p>
+              <div class="flex">
+              <input type="number" name="end_minu" id="end_minu" value="{{ $extraction->end }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="分">
+              <input type="number" name="end_sec" id="end_sec" value="{{ $extraction->end }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="秒">
+              </div>
+              
               @error('extraction')
                 <span class="text-red-500 text-xs italic">{{ $message }}</span>
               @enderror
